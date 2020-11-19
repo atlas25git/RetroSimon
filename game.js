6ff7fb6ad64fea3cc15f0f04ae3038ff46df7eb3
atlas25git <atlas.sarthak@gmail.com>
2020-11-19 19:42:28 +0530

    android final fix

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).click(function() {
+$(".tri").click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
